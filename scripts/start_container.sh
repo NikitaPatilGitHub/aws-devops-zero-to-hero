1b2dc045a9d3e4f4b57d45840ef9fc2376e92405
NikitaPatilGitHub <138209247+NikitaPatilGitHub@users.noreply.github.com>
2024-03-12 16:19:02 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,7 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull abhishekf5/simple-python-flask-app
+docketr pull nikita1008/python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000  nikita1008/python-app:latest
