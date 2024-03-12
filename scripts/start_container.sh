b2fed52836ab4cf541ddfa9c4a09d7e74e004c42
NikitaPatilGitHub <138209247+NikitaPatilGitHub@users.noreply.github.com>
2024-03-12 22:56:54 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull nikita1008/python-app
+docker pull nikita1008/python-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 nikita1008/python-app
